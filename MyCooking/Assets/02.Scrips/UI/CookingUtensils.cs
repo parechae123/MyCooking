4fdd7a01de3be882905fd0eee7b5dc38193d388d
junhee4613 <junhee000123@naver.com>
2023-06-07 01:14:59 +0900

    숟가락 코드 바꾸고 푸쉬

--- MyCooking/Assets/02.Scrips/UI/CookingUtensils.cs
+++ MyCooking/Assets/02.Scrips/UI/CookingUtensils.cs
@@ -8,6 +8,7 @@ public class CookingUtensils : MonoBehaviour
     public List<GameObject> cook = new List<GameObject>();
     public FryFan fryfan;
     public int hitnum = 0;
+    public Vector3 me;
     // Start is called before the first frame update
     void Start()        //음식 터치했을 때 음식이 완성되게 하기 및 해당 도구가 거치대에 걸어지게 하기
     {
@@ -19,39 +20,46 @@ public class CookingUtensils : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*cook.Add(GameManager.GMinstatnce().ingredientCookIndex.Dequeue());              //큐에서 빼낸 오브젝트를 리스트에 넣어준다.*/
-        Debug.DrawRay(transform.position, transform.forward * 0.5f, Color.red);
+        me = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
+
+        Debug.DrawRay(transform.position, transform.forward * 0.7f, Color.red);
         RaycastHit hitInfo;
-        if (Physics.Raycast(this.transform.position, transform.forward, out hitInfo, 0.7f, layerMask))
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, 0.7f, layerMask))
         {
+            Debug.Log("반응");
             if (hitInfo.collider.transform.childCount > 0)
             {
-                if (hitInfo.collider.transform.GetChild(0).gameObject.layer == layerMask)
+                GameObject childObject = hitInfo.collider.transform.GetChild(0).gameObject;
+                if (childObject.layer == layerMask)
                 {
-                    hitInfo.collider.transform.GetChild(0).gameObject.SetActive(false);
                     Debug.Log("반응2");
+                    childObject.SetActive(false);
+                    Debug.Log("반응3");
                 }
             }
 
             if (hitInfo.collider.transform.childCount > 1)
             {
-                if (hitInfo.collider.transform.GetChild(1).gameObject.layer == layerMask)
+                Debug.Log("반응2");
+                GameObject childObject = hitInfo.collider.transform.GetChild(1).gameObject;
+                if (childObject.layer == layerMask)
                 {
-                    hitInfo.collider.transform.GetChild(1).gameObject.SetActive(false);
-                    Debug.Log("반응2");
+                    childObject.SetActive(false);
+                    Debug.Log("반응3");
                 }
             }
 
             if (hitInfo.collider.transform.childCount > 2)
             {
-                if (hitInfo.collider.transform.GetChild(2).gameObject.layer == layerMask)
+                GameObject childObject = hitInfo.collider.transform.GetChild(2).gameObject;
+                if (childObject.layer == layerMask)
                 {
-                    hitInfo.collider.transform.GetChild(2).gameObject.SetActive(false);
-                    Debug.Log("반응2");
+                    childObject.SetActive(false);
+                    Debug.Log("반응3");
                 }
             }
 
-            Debug.Log("반응");
+            Debug.Log("반응" + hitInfo.transform.name);
         }
     }
 }
